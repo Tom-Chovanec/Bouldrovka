482281cbdb087ad9de83111ca07295415aad06cd
Tom-Chovanec <118990261+Tom-Chovanec@users.noreply.github.com>
2024-09-12 18:16:33 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-sudo  ./gradlew assembleDebug
-adb install /home/tom/Creative/cpp/Bouldrovka/app/build/outputs/apk/debug/app-debug.apk
-adb shell monkey -p com.bouldrovka.app -c android.intent.category.LAUNCHER 1
-

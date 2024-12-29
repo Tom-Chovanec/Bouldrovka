3725ab1c07996bba99cd934e9f0bbcb0a6f8b8a1
Tom <chovanectomas07@gmail.com>
2024-12-29 14:10:26 +0100

    rewrite + migration to sdl3

--- app/build/intermediates/cxx/Debug/29445a3u/obj/local/arm64-v8a/objs-debug/SDL3_image/src/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-/home/tom/projects/cpp/bouldrovka/app/build/intermediates/cxx/Debug/29445a3u/obj/local/arm64-v8a/objs-debug/SDL3_image/src/IMG_WIC.o: \
-  /home/tom/projects/cpp/bouldrovka/app/jni/SDL_image/src/IMG_WIC.c
